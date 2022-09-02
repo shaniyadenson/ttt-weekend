96f2e46173d8c9d52c4a6182a2f037275d5a1d35
sdenson <shaniyad97@gmail.com>
2022-09-02 12:11:32 -0500

    creates init function

--- js/app.js
+++ js/app.js
@@ -4,15 +4,35 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board, turn, winner
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const squareEls = document.querySelector(".boardSquare")
+const messageEl = document.querySelector("#message")
+console.log(squareEls);
+console.log(messageEl);
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init()
+
+function init() {
+  board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  turn = 1
+  winner = null
+
+render()
+}
+
+// function render() {
+
+// board.forEach((squareEls, idx) => console.log(squareEls))
+
+
+// }
 
